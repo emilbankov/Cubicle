9097126bacaae9c70fb1d44e038e39d6de7570b2
emilbankov <emilbankov17d@gmail.com>
2023-10-15 15:40:35 +0300

    Cubicle project update
    Cleared dependencies, made cube create endpoint work with DB

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,38 +1,10 @@
-const uniqid = require("uniqid");
+const Cube = require("../models/Cube.js");
 
-const cubes = [
-    {
-        id: '51r28k8lnhiqeuq',
-        name: 'Eco-Dark',
-        description: 'The best looking rubix cube out there!',
-        imageUrl: 'https://thingsidesire.com/wp-content/uploads/2018/06/Eco-Dark-Rubik%E2%80%99s-Cube2.jpg',
-        difficultyLevel: 6
-    },
-    {
-        id: '51r28k8lnhitt68',
-        name: 'Pyraminx',
-        description: 'Amazing egyptian pyramid style cube!',
-        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61izOzq%2BBAL._SY355_.jpg',
-        difficultyLevel: 1
-    },
-    {
-        id: '51r28k8lnhitt68',
-        name: 'Megaminx',
-        description: 'Interesting form for a cube!',
-        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61HpQqVQ37L._SY355_.jpg',
-        difficultyLevel: 3
-    }
-];
+const cubes = [];
 
-exports.create = (cubeData) => {
-    const newCube = {
-        id: uniqid(),
-        ...cubeData,
-    };
-
-    cubes.push(newCube);
-
-    return newCube;
+exports.create = async (cubeData) => {
+    const cube = await Cube.create(cubeData);
+    return cube;
 }
 
 exports.getAll = (search, from, to) => {
