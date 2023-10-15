fe8779392cf5d59233f0cd66976dbe99f2ee78ef
emilbankov <emilbankov17d@gmail.com>
2023-10-15 18:15:20 +0300

    Cubicle project update
    Get accessories for each cube without owned ones.

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -36,8 +36,12 @@ router.get("/:cubeId/details", async (req, res) => {
 router.get("/:cubeId/attach-accessory", async (req, res) => {
     const { cubeId } = req.params;
     const cube = await cubeService.getSingleCube(cubeId).lean();
-    const accessories = await accessoryService.getAll().lean();
+
+    const accessoryIds = cube.accessories ? cube.accessories.map((a) => a._id) : [];
+    const accessories = await accessoryService.getWithoutOwned(accessoryIds).lean();
+
     const hasAccessories = accessories.length > 0;
+
 
     res.render("accessory/attach", { cube, accessories, hasAccessories });
 });
--- src/services/accessoryService.js
+++ src/services/accessoryService.js
@@ -4,3 +4,7 @@ const Accessory = require("../models/Accessory.js");
 exports.create = (accessoryData) => Accessory.create(accessoryData);
 
 exports.getAll = () => Accessory.find();
+
+exports.getWithoutOwned = (accessoryIds) => {
+    return Accessory.find({ _id: { $nin: accessoryIds } });
+}
