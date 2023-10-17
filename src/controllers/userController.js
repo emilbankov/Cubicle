213241ca2a95e5038f5ac6d4ed4d94d2099f7afa
emilbankov <emilbankov17d@gmail.com>
2023-10-17 15:55:07 +0300

    Cubicle project update
    Register user and save it to DB

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,23 @@
 const router = require("express").Router();
+const userService = require("../services/userService.js");
 
 router.get("/register", (req, res) => {
-    res.render("user/register")
+    res.render("user/register");
 });
 
+router.post("/register", async (req, res) => {
+    const { username, password, repeatPassword } = req.body;
+    await userService.register({ username, password, repeatPassword });
+    
+    res.redirect("/users/login");
+  });
+
 router.get("/login", (req, res) => {
-    res.render("user/login")
+    res.render("user/login");
 });
+
+// router.post("/login", (req, res) => {
+//     res.render("user/login");
+// });
 
 module.exports = router;
