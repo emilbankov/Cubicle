7e8cdb301f15da6c859793a2b9743c7f3cd18c23
emilbankov <emilbankov17d@gmail.com>
2023-10-17 17:26:28 +0300

    Cubicle project update
    Edit and delete pages initialize

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -55,4 +55,12 @@ router.post("/:cubeId/attach-accessory", async (req, res) => {
     res.redirect(`/cubes/${cubeId}/details`);
 });
 
+router.get("/:cubeId/edit", async (req, res) => {
+    res.render("cube/edit");
+});
+
+router.get("/:cubeId/delete", async (req, res) => {
+    res.render("cube/delete");
+});
+
 module.exports = router;
