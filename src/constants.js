0e8ab2811ed053eaa416deccb19685e4959c03d7
emilbankov <emilbankov17d@gmail.com>
2023-10-17 17:00:59 +0300

    Cubicle project update
    Provide secret for jwt

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 const constants = {
     PORT: 5050,
     URL: "mongodb://127.0.0.1:27017/cubicle-sep-2023",
+    SECRET: "MyCoolSecret",
 }
 
 module.exports = constants;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,31 @@
 const User = require("../models/User.js")
+const bcrypt = require("bcrypt");
+const jwt = require("../lib/jwt.js");
+const { SECRET } = require("../constants.js");
 
 exports.register = (userData) => {
     return User.create(userData);
+};
+
+exports.login = async (username, password) => {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+        throw new Error("Invalid username or password!");
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error("Invalid username or password!");
+    }
+
+    const payload = {
+        _id: user._id,
+        username: user.username,
+
+    };
+    const token = await jwt.sign(payload, SECRET, { expiresIn: "3d" });
+
+    return token;
 };
